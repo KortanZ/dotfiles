1d32e8f44149f94adf2ad998293548c163534648
Kortan <kortanzh@gmail.com>
2024-01-05 21:20:04 +0800

    [nvim] update lualine lsp integration

--- nvim/.config/nvim/plugin/lualine.lua
+++ nvim/.config/nvim/plugin/lualine.lua
@@ -5,14 +5,21 @@ require('lualine').setup{
         component_separators = { left = '|', right = '|' }
     },
     sections = {
-        lualine_c = {'lsp_progress'},
+        lualine_c = {require('lsp-progress').progress}
     },
     inactive_sections = {
-        lualine_c = {'lsp_progress'},
-        lualine_x = {'location'},
+        lualine_c = {require('lsp-progress').progress}
     },
     tabline = {
         lualine_a = {'buffers'},
         lualine_z = {'tabs'}
     },
 }
+
+-- listen lsp-progress event and refresh lualine
+vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+vim.api.nvim_create_autocmd("User", {
+  group = "lualine_augroup",
+  pattern = "LspProgressStatusUpdated",
+  callback = require("lualine").refresh,
+})
