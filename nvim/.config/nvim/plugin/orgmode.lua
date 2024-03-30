1a604e312900380e9b01aaf2cdf57fe631453098
Kortan <kortanzh@gmail.com>
2024-03-30 15:26:24 +0800

    [nvim] orgmode config

--- nvim/.config/nvim/plugin/orgmode.lua
+++ nvim/.config/nvim/plugin/orgmode.lua
@@ -4,4 +4,9 @@ require('orgmode').setup_ts_grammar()
 require('orgmode').setup({
   org_agenda_files = '~/org/*',
   org_default_notes_file = '~/org/refile.org',
+  mappings = {
+      org = {
+          org_toggle_checkbox = '<prefix>tt',
+      },
+
 })
