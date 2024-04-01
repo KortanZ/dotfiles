bcd4de1548006ecef32c8cd40cb4347f14d5d50d
Kortan <kortanzh@gmail.com>
2024-04-01 10:01:46 +0800

    [nvim] update orgmode

--- nvim/.config/nvim/plugin/orgmode.lua
+++ nvim/.config/nvim/plugin/orgmode.lua
@@ -1,6 +1,3 @@
--- Load custom treesitter grammar for org filetype
-require('orgmode').setup_ts_grammar()
-
 require('orgmode').setup({
   org_agenda_files = '~/org/*',
   org_default_notes_file = '~/org/refile.org',
