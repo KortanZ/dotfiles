bf8ce76dd59f2181c12e200fda850fb421495775
Kortan <kortanzh@gmail.com>
2024-06-24 23:11:16 +0800

    [nvim] lsp keybind change to fzf-lua func

--- nvim/.config/nvim/lua/keymap.lua
+++ nvim/.config/nvim/lua/keymap.lua
@@ -45,20 +45,21 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
-    map('n', 'gD', vim.lsp.buf.declaration, opts)
-    map('n', 'gd', vim.lsp.buf.definition, opts)
     map('n', 'K', vim.lsp.buf.hover, opts)
-    map('n', 'gi', vim.lsp.buf.implementation, opts)
+    map('n', 'gD', require("fzf-lua").lsp_declarations, opts)
+    map('n', 'gd', require("fzf-lua").lsp_definitions, opts)
+    map('n', 'gi', require("fzf-lua").lsp_implementations, opts)
+    map('n', 'gr', require("fzf-lua").lsp_references, opts)
+
     map('n', '<C-k>', vim.lsp.buf.signature_help, opts)
     map('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
     map('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
     map('n', '<space>wl', function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, opts)
-    map('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    map('n', '<space>D', require("fzf-lua").lsp_typedefs, opts)
     map('n', '<space>rn', vim.lsp.buf.rename, opts)
-    map({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    map('n', 'gr', vim.lsp.buf.references, opts)
+    map({ 'n', 'v' }, '<space>ca', require("fzf-lua").lsp_code_actions, opts)
     map('n', '<space>f', function()
       vim.lsp.buf.format { async = true }
     end, opts)
